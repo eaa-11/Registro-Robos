b6e601dc1c1fcc6006ecd2a1bf875692e69f3b26
Alejandra Adames <eaanny2611@gmail.com>
2021-08-05 21:46:47 -0400

    Adding ApplicationDbContext as a model

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Prueba.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public virtual DbSet<Robo> Robos { get; set; }
+    }
+}
